d92651e4fe5b2d30cc028e6c4b92377f2f3889a1
asimovSO <svoleg2002@gmail.com>
2022-12-31 14:11:21 +0200

    add pagintaion ui

---
+++ src/components/Pagination.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-pagination
+    :length="8"
+    color="#167"
+    dark
+    :value="1"
+    v-model="currentPage"
+    circle
+  ></v-pagination>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentPage: 1,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
