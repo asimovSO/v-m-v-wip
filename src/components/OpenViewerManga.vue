0ff8d5a5a8e5aeb56bae5f76213aef3aafa6c28a
asimovSO <svoleg2002@gmail.com>
2023-01-01 21:03:11 +0200

    fixed viewer and some functionality

--- src/components/OpenViewerManga.vue
+++ src/components/OpenViewerManga.vue
@@ -1,31 +1,49 @@
 <template>
-  <div class="viewer-manga">
+  <div class="viewer-manga" @click.stop="openSettings">
     <div class="images">
-        <img
-      v-for="(img, index) in images"
-      :key="index"
-      :src="img"
-      
-      class="chapter-img"
-      @click="openViewer = true"
+      <img
+        v-for="(img, index) in images"
+        :key="index"
+        :src="img"
+        class="chapter-img"
+      />
+    </div>
+    <div
+      class="settings-viewer d-flex justify-end black p-8"
+      v-if="getStateSettings"
     >
+      <v-icon color="white" @click.stop="closeViewer"> mdi-close </v-icon>
+      <!--close the modal viewer images -->
     </div>
   </div>
 </template>
 
 <script>
+import { mapMutations, mapGetters } from "vuex";
+
 export default {
   name: "OpenViewerManga",
   props: ["images"],
   data() {
-    return {
-      openViewer: false,
-    };
+    return {};
+  },
+  methods: {
+    ...mapMutations(["openSettings", "closeViewer"]),
+  },
+  computed: {
+    ...mapGetters(["getStateSettings"]),
   },
 };
 </script>
 
 <style>
+.settings-viewer {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  padding: 1rem;
+  z-index: 9999;
+}
 .viewer-manga {
   position: fixed;
   top: 0;
@@ -35,26 +53,26 @@ export default {
   width: 100%;
   overflow: auto;
 }
-.images{
-   
-    position: absolute;
-    right: 50%;
-    max-width: 550px;
-    transform: translateX(50%);
+.images {
+  position: absolute;
+  right: 50%;
+  max-width: 750px;
+  transform: translateX(50%);
 }
-.chapter-img{
-    object-fit: contain; 
-    display: block;
-    width: 100%;
+.chapter-img {
+  object-fit: contain;
+  display: block;
+  width: 100%;
 }
 
-@media (orientation: portrait){
-    .images{
-        height: 100vh;   
-    }
-    .chapter-img{
-        width: 100vw;
-        
-    }
+@media (orientation: portrait) {
+  .images {
+    height: 100vh;
+    width: 100vw;
+  }
+  .chapter-img {
+    width: 100%;
+    display: block;
+  }
 }
 </style>
