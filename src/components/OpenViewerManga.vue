61baf652678a7f81709370546dd177d337cd0c1f
asimovSO <svoleg2002@gmail.com>
2023-01-01 16:37:49 +0200

    add simple viewer for manga images

---
+++ src/components/OpenViewerManga.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="viewer-manga">
+    <div class="images">
+        <img
+      v-for="(img, index) in images"
+      :key="index"
+      :src="img"
+      
+      class="chapter-img"
+      @click="openViewer = true"
+    >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OpenViewerManga",
+  props: ["images"],
+  data() {
+    return {
+      openViewer: false,
+    };
+  },
+};
+</script>
+
+<style>
+.viewer-manga {
+  position: fixed;
+  top: 0;
+  z-index: 222;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.601);
+  width: 100%;
+  overflow: auto;
+}
+.images{
+   
+    position: absolute;
+    right: 50%;
+    max-width: 550px;
+    transform: translateX(50%);
+}
+.chapter-img{
+    object-fit: contain; 
+    display: block;
+    width: 100%;
+}
+
+@media (orientation: portrait){
+    .images{
+        height: 100vh;   
+    }
+    .chapter-img{
+        width: 100vw;
+        
+    }
+}
+</style>
